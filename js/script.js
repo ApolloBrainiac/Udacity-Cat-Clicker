794e5bc84fdeb452a4545fa8de4ee3a3deedbd79
Andrew M. Perez <AxMPerez@gmail.com>
2018-02-23 18:17:09 -0500

    Begin writing JS functionality for Cat Clicker

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // alert("Hello World!");
+var elem = document.getElementById('cat');
+var pokes = 0;
+
+elem.addEventListener('click', function(){
+	pokes += 1;
+	alert("I said don't! You've poke the poor cat " + pokes + " times, now!");
+}, false);
